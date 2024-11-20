95d9ef0b61ed786d981b8006b44ca1b04279537a
digydigdogdead <carlyle.laurent@gmail.com>
2024-11-20 12:25:55 +0000

    Created a UserData class with functions for getting name, age, height, and weight

---
+++ Exercise2.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    internal class Exercise2
+    {
+        public static void HealthInsuranceDataInput()
+        {
+            Console.WriteLine("\n--- Thank you for choosing Blummin Health insurance! ---\n");
+            Console.WriteLine("Please input your data:");
+
+            // Name
+            string name = UserData.GetName();
+
+            // Age
+            int age = UserData.GetAge();
+
+            // Height
+            double height = UserData.GetHeight();
+
+            // Weight
+            double weight = UserData.GetWeight();
+
+            // Employment
+            Console.WriteLine("Are you employed?: ");
+            string employed = Console.ReadLine();
+
+            // Occupation
+            Console.WriteLine("What is your occupation?: ");
+            string occupation = Console.ReadLine();
+
+            // Student
+            Console.WriteLine("Are you a student?: ");
+            string isStudent = Console.ReadLine();
+
+            Console.WriteLine("\nThank you, " + name + ", for providing your information!");
+            Console.WriteLine("Your Blummin monthly subscription is: £"
+                    + CalculateSubscriptionCharge(Int32.Parse(age), Double.Parse(height), Double.Parse(weight), occupation, bool.Parse(isStudent)));
+        }
+
+        private static double CalculateSubscriptionCharge(int age, double height, double weight, string occupation, bool isStudent)
+        {
+            List<double> factors = new List<double>()
+        {
+            0.5D, // Base rate
+            CalculateAgeFactor(age),
+            CalculateHeightFactor(height),
+            CalculateWeightFactor(weight),
+            occupation.ToLower() == "doctor" ? 0.9D : 1.0D,
+            isStudent ? 0.8D : 1.0D
+        };
+
+            return factors.Aggregate(1.0, (a, b) => a * b);
+        }
+
+        private static double CalculateAgeFactor(int age)
+        {
+            return Math.Max(100 + (age - 18) * 5, 1);
+        }
+
+        private static double CalculateHeightFactor(double height)
+        {
+            return Math.Abs(Math.Pow(height, 2) - 2);
+        }
+
+        private static double CalculateWeightFactor(double weight)
+        {
+            return 2 * weight;
+        }
+    }
+}
---
+++ UserData.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ExceptionHandling
+{
+    internal class UserData
+    {
+        public static string GetName()
+        {
+            Console.WriteLine("Please give us your full name.");
+            string? result = Console.ReadLine();
+            if (result is null || !result.Contains(" ") || result[0] == ' ' || result[^1] == ' ') 
+            {
+                Console.WriteLine("Name invalid, please try again.");
+            }
+            result = GetName();
+            return result;
+        }
+
+        public static int GetAge()
+        {
+            Console.WriteLine("Enter your age: ");
+            string? input = Console.ReadLine();
+            int age = 0;
+            bool valid = Int32.TryParse(input, out age);
+            if (!valid || age < 0 || age > 120)
+            {
+                Console.WriteLine("Age is not valid. Please try again.");
+                age = GetAge();
+            }
+            return age;
+        }
+
+        internal static double GetHeight()
+        {
+            Console.WriteLine("Enter your height (in metres): ");
+            string? input = Console.ReadLine();
+            double height = 0;
+            bool valid = Double.TryParse(input, out height);
+            if (!valid || height < 0 || height > 2.72)
+            {
+                Console.WriteLine("Height is invalid, please try again.");
+                height = GetHeight();
+            }
+            return height;
+        }
+        internal static double GetWeight()
+        {
+            Console.WriteLine("Enter your weight (in kg): ");
+            string? input = Console.ReadLine();
+            double weight = 0;
+            bool valid = Double.TryParse(input, out weight);
+            if (!valid || weight < 0 || weight > 650)
+            {
+                Console.WriteLine("Weight is invalid, please try again.");
+                weight = GetWeight();
+            }
+            return weight;
+        }
+
+        internal enum Employed
+        {
+            YES,
+            NO
+        }
+    }
+}
